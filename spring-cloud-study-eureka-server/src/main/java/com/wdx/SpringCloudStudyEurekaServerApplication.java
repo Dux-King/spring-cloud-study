51be08cf3abb0188a26513dc508fb992fbdac148
dux <a>
2017-08-22 17:47:09 +0800

    init

---
+++ spring-cloud-study-eureka-server/src/main/java/com/wdx/SpringCloudStudyEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.wdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStudyEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStudyEurekaServerApplication.class, args);
+	}
+}
